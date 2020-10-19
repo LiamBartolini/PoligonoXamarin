a66ee72ac0bf8ec52a270fc2e4cff96b7928937d
LiamBartolini <liamabartolini@gmail.com>
2020-10-19 18:41:13 +0200

    First commit

---
+++ Bartolini.Liam.4H.PoligonoXml/MainPage.xaml.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Bartolini.Liam._4H.PoligonoXml.Models;
+
+namespace Bartolini.Liam._4H.PoligonoXml
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Calcola(object sender, EventArgs e)
+        {
+            bool f = true;
+            string strNlati = "";
+            string strLlati = "";
+
+            while (f)
+            {
+                strNlati = NumeroLati.Text;
+                strLlati = lLAti.Text;
+                f = InputCheck(strNlati);
+            }
+
+            Poligono figura = new Poligono(Convert.ToDouble(strNlati), Convert.ToDouble(strLlati));
+
+            figura.Apotema();
+            figura.Fisso();
+
+            figura.Perimetro();
+            figura.Area();
+
+            //stampa per il nome del calcolo
+            lblRisultatoCol1.Text = $"Risultati:\r\nArea: \r\nPerimetro: \r\nApotema: \r\nNumero fisso: \r\nNome: ";
+
+            //stampa per i risultati
+            lblRisultatoCol2.Text = $"\r\n{figura.area:n2} cm^2\r\n{figura.perimetro:n2} cm\r\n{figura.apotema:n2} cm\r\n{figura.fisso:n2}\r\n{figura.Nome()}";
+        }
+
+        private bool InputCheck(string nLati)
+        {
+            bool f = false;
+
+            if (Convert.ToInt32(nLati) < 3)
+                f = true;
+
+            return f;
+        }
+
+        private void Confronta(object sender, EventArgs e)
+        {
+            string strNLati = NumeroLati.Text;
+            string strLLati = lLAti.Text;
+
+            double Nlati = Convert.ToDouble(strNLati);
+            double Llati = Convert.ToDouble(strLLati);
+
+            Poligono figura = new Poligono(Nlati, Llati);
+            Poligono personale = new Poligono(4, 7);
+
+            lblRisultatoCol1.Text = $"Poligo di confronto: \r\nLati: \r\nLunghezza lato: \r\n\r\nL'esito del confronto";
+            lblRisultatoCol2.Text = $"\r\n{personale.nLati}\r\n{personale.L_lati}\r\n\r\n{figura.Confronta(personale)}";
+            DrawPrint(Nlati);
+        }
+
+        private void DrawPrint(double nlati)
+        {
+            switch(nlati)
+            {
+                case 3:
+                    triangolo.IsVisible = true;
+                    break;
+                case 4:
+                    quadrato.IsVisible = true;
+                    break;
+                case 5:
+                    pentagono.IsVisible = true;
+                    break;
+                case 6:
+                    esagono.IsVisible = true;
+                    break;
+                case 7:
+                    ettagono.IsVisible = true;
+                    break;
+                case 8:
+                    ottagono.IsVisible = true;
+                    break;
+                case 9:
+                    ennagono.IsVisible = true;
+                    break;
+                case 10:
+                    decagono.IsVisible = true;
+                    break;
+                case 11:
+                    endecagono.IsVisible = true;
+                    break;
+                case 12:
+                    dodecagono.IsVisible = true;
+                    break;
+            }
+
+            quadratoPersonale.IsVisible = true;
+        }
+    }
+}
---
+++ Bartolini.Liam.4H.PoligonoXml/Models/Poligono.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bartolini.Liam._4H.PoligonoXml.Models
+{
+    class Poligono
+    {
+        public double nLati;
+        public double L_lati;
+        public double apotema;
+        public double perimetro;
+        public double area;
+        public double fisso;
+
+        public Poligono()
+        {
+            //costruisce il quadrato di lato 1
+            nLati = 4;
+            L_lati = 1;
+        }
+
+        //costruttore con nLati e Llati
+        public Poligono(double lati, double lLati)
+        {
+            nLati = lati;
+            L_lati = lLati;
+        }
+
+        //costruttore di un poligono regolare di lato 1
+        public Poligono(int lati)
+        {
+            L_lati = 1;
+            nLati = lati;
+        }
+
+        public void Area()
+        {
+            this.area = this.perimetro * this.fisso / 2;
+        }
+
+        public void Perimetro()
+        {
+            this.perimetro = this.nLati * this.L_lati;
+        }
+
+        public void Apotema()
+        {
+            this.apotema = this.L_lati / (2 * Math.Tan(Math.PI / this.nLati));
+        }
+
+        public void Fisso()
+        {
+            this.fisso = this.apotema / this.L_lati;
+        }
+
+        public string Nome()
+        {
+            string strR;
+
+            if (this.nLati < 3)
+                return "Non è un poligono";
+
+            string[] array = new string[] { "triangolo", "quadrato", "pentagono", "esagono", "ettagono", "ottagono", "ennagono", "decagono", "endecagono", "dodecagono" };
+
+            strR = array[(int)this.nLati - 3];
+
+            return strR;
+        }
+
+        public string Confronta(Poligono p2)
+        {
+            if (this.nLati == p2.nLati)
+            {
+                if (this.L_lati == p2.L_lati)
+                    return "Uguale";
+                if (this.L_lati < p2.L_lati)
+                    return "Più piccolo";
+                else
+                    return "Più grande";
+            }
+            else
+                return "Non confrontabili";
+        }
+    }
+}
